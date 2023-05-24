c1f042c9f31a881161ea0aee39b8a95b98d9e4f8
kimmoonju-102 <kimmun3549@naver.com>
2023-05-24 15:00:21 +0900

    01. 제로초 - 끝말잇기
    - 틀렸을 때 오류 표시하기
    - input 초기화
    - input focus

--- 끝말잇기/index.js
+++ 끝말잇기/index.js
@@ -2,6 +2,7 @@ const number = Number(prompt('몇 명이 참가하나요?'));
 const $button = document.querySelector('button');
 const $input = document.querySelector('input');
 const $word = document.querySelector( '#word' );
+const $order = document.querySelector( '#order' );
 let word; // 제시어
 let newWord; // 새로 입력한 단어
 
@@ -10,27 +11,31 @@ const onClickButton = () => {
     // 비어 있다.
     word = newWord; // 입력한 단어가 제시어가 된다.
     $word.textContent = word;
-    $input.value = ''; // input 같은 입력값들은 value를 바꿔야 함.
-    cosnt order = parseInt($order.textContent); // 현재 순서
+    const order = parseInt($order.textContent); // 현재 순서
     if (order + 1 > number) {
       $order.textContent = 1;
     } else {
       $order.textContent = order + 1;
     }
+    $input.value = ''; // input 같은 입력값들은 value를 바꿔야 함.
+    $input.focus();
   } else {
     // 비어 있지 않다.
     if (word[word.length -1] === newWord[0]) { // 올바른가
       word = newWord; // 입력한 단어가 제시어가 된다.
       $word.textContent = word;
-      $input.value = ''; // input 같은 입력값들은 value를 바꿔야 함.
-      cosnt order = parseInt($order.textContent); // 현재 순서
+      const order = parseInt($order.textContent); // 현재 순서
       if (order + 1 > number) {
         $order.textContent = 1;
       } else {
         $order.textContent = order + 1;
       }
+      $input.value = ''; // input 같은 입력값들은 value를 바꿔야 함.
+      $input.focus();
     } else { // 올바르지 않은가
-
+      alert('올바르지 않은 단어입니다');
+      $input.value = ''; // input 같은 입력값들은 value를 바꿔야 함.
+      $input.focus();
     }
   }
 };
