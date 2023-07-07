cce5510b86a907f019d4ff6a1d0f1863e779e8dc
kimmoonju-102 <kimmun3549@naver.com>
2023-07-07 16:52:24 +0900

    03. 제로초 - 숫자야구
    - 몇 볼 몇 스트라이크인지 확인

--- 숫자야구/main.js
+++ 숫자야구/main.js
@@ -20,6 +20,7 @@ console.log(answer);
   console.log('서브밋');
 }); */
 const tries = [];
+// 입력값 문제있음 검사하는 코드
 function checkInput(input) {
   if (input.length !== 4) {
     return alert('4자리 숫자를 입력해 주세요.');
@@ -31,16 +32,40 @@ function checkInput(input) {
     return alert('이미 시도한 값입니다.');
   }
   return true;
-} // 검사하는 코드
+}
 
 $form.addEventListener('submit', (event) => {
   event.preventDefault();
   const value = $input.value; // value 변수에 저장하고
   $input.value = ''; // 글자값 지워줌
-  if (checkInput(value)) {
-    // 입력값 문제없음
-    tries.push(value);
-  } else {
-    // 에러 있음.
-  }// input값을 검사하는 코드
+  // 입력값 문제있음
+  if(!checkInput(value)) {
+    return;
+  }
+  // 입력값 문제없음 검사하는 코드
+  if (answer.join('') === value) {
+    $logs.textContent = '홈런!';
+    return;
+  }
+  if (tries.length >= 9) {
+    const message = document.createTextNode(`패배! 정답은 ${answer.join('')}`);
+    $logs.appendChild(message);
+    return;
+  }
+  // 몇 스트라이크 몇 볼인지 검사
+  let strike = 0;
+  let ball = 0;
+  // answer: 3146, value: 1234
+  for (let i = 0; i < answer.length; i++) {
+    const index = value.indexOf(answer[i]);
+    if (index > -1) { //일치하는 숫자 발견
+      if (index === i) { // 자릿수도 같음
+        strike += 1;
+      } else { // 숫자도 같음
+        ball += 1;
+      }
+    }
+  }
+  $logs.append(`${value}: ${strike} 스트라이크 ${ball} 볼`, document.createElement('br'));
+  tries.push(value); // 시도할때마다 tries에 기록
 });
