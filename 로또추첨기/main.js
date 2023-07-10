2d7c2a63589feaa59f649c77229021591d19a4ff
kimmoonju-102 <kimmun3549@naver.com>
2023-07-10 16:22:04 +0900

    04. 제로초 - 공 뽑기
    - for문 while문 정리
    - 랜덤으로 공 뽑기
    - 배열에 쓰이는 메서드 정리

---
+++ 로또추첨기/main.js
@@ -0,0 +1,22 @@
+const candidate = Array(45).fill().map((v, i) => i + 1); // 1 ~ 45까지 뽑는 코드
+const shuffle = [];
+while (candidate.length > 0){
+  const random = Math.floor(Math.random() * candidate.length); // 무작위 인덱스 뽑기
+  const spliceArray = candidate.splice(random, 1); // 뽑은 값은 배열에 들어 있음. splice에 return값을 넣음
+  const value = spliceArray[0]; // 배열에 들어 있는 값을 꺼내어
+  shuffle.push(value); // shuffle 배열에 넣기
+}
+console.log(shuffle);
+
+// for문 바꾸기
+/* for (let i = candidate.length; i > 0; i--;){
+  const random = Math.floor(Math.random() * i); // 무작위 인덱스 뽑기
+  const spliceArray = candidate.splice(random, 1); // 뽑은 값은 배열에 들어 있음. splice에 return값을 넣음
+  const value = spliceArray[0]; // 배열에 들어 있는 값을 꺼내어
+  shuffle.push(value); // shuffle 배열에 넣기
+}
+console.log(shuffle); */
+
+const winBall = shuffle.slice(0, 6).sort((a, b) => a - b); // 앞에서 6개 자르고 정렬
+const bonus = shuffle[6]; // 7번째 공 가져오기
+console.log(winBall, bonus);
