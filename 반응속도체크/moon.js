ef95e2db9051a9ccd40187f1524121c8f4fd3a5a
kimmoonju-102 <kimmun3549@naver.com>
2023-07-19 22:31:35 +0900

    05. 제로초 - 성급한 클릭 막기
    - 필요 없는 구간에선 clearTimout을 사용하여 타이머 제거

--- 반응속도체크/moon.js
+++ 반응속도체크/moon.js
@@ -4,13 +4,14 @@ const $result = document.querySelector('#result');
 let startTime;
 let endTime;
 const records = [];
+let timeoutId;
 $screen.addEventListener('click', (event) => {
   if (event.target.classList.contains('waiting')) { //파랑
     $screen.classList.remove('waiting');
     $screen.classList.add('ready');
     $screen.textContent = '초록색이 되면 클릭하세요';
     // 빨간 화면을 클릭하기 전에 타이머 시작
-    setTimeout(function() {
+    timeoutId = setTimeout(function() {
       startTime = new Date();
       $screen.classList.remove('ready');
       $screen.classList.add('now');
@@ -18,7 +19,10 @@ $screen.addEventListener('click', (event) => {
       // 첫시간재기
     }, Math.floor(Math.random() * 1000) + 2000); // 2초에서 3초 사이 2000~3000사이 수
   } else if (event.target.classList.contains('ready')) { //빨강
-
+    clearTimeout(timeoutId);
+    $screen.classList.remove('ready');
+    $screen.classList.add('waiting');
+    $screen.textContent = '너무 성급하시군요!';
   } else if (event.target.classList.contains('now')) { //초록
     endTime = new Date();
     const current = endTime - startTime;
