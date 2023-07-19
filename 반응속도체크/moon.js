3514b3a73013d06af868f97dac4b01ab3f1e8bf9
kimmoonju-102 <kimmun3549@naver.com>
2023-07-19 21:53:35 +0900

    05. 제로초  - 반응속도체크
    - new Date() 사용하기
    - reduce 매서드 사용해서 평균 속도 구하기

---
+++ 반응속도체크/moon.js
@@ -0,0 +1,35 @@
+const $screen = document.querySelector('#screen');
+const $result = document.querySelector('#result');
+
+let startTime;
+let endTime;
+const records = [];
+$screen.addEventListener('click', (event) => {
+  if (event.target.classList.contains('waiting')) { //파랑
+    $screen.classList.remove('waiting');
+    $screen.classList.add('ready');
+    $screen.textContent = '초록색이 되면 클릭하세요';
+    // 빨간 화면을 클릭하기 전에 타이머 시작
+    setTimeout(function() {
+      startTime = new Date();
+      $screen.classList.remove('ready');
+      $screen.classList.add('now');
+      $screen.textContent = '클릭하세요';
+      // 첫시간재기
+    }, Math.floor(Math.random() * 1000) + 2000); // 2초에서 3초 사이 2000~3000사이 수
+  } else if (event.target.classList.contains('ready')) { //빨강
+
+  } else if (event.target.classList.contains('now')) { //초록
+    endTime = new Date();
+    const current = endTime - startTime;
+    records.push(current);
+    const average = records.reduce((a, c) => a + c) / records.length;
+    endTime = new Date();
+    $result.textContent = `현재 ${current}ms, 평균 : ${average}ms`;
+    startTime = null; // 초기화
+    endTime = null; // 초기화
+    $screen.classList.remove('now');
+    $screen.classList.add('waiting');
+    $screen.textContent = '클릭해서 시작하세요';
+  }
+})
